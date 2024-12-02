a6b558055100910432e2ee0f3e289928ae04f4e2
student <>
2024-12-02 12:35:11 +0900

    今日の分（カメラ上固定完了）

--- Source/Smith/Public/MyPlayerCharacter.h
+++ Source/Smith/Public/MyPlayerCharacter.h
@@ -6,6 +6,10 @@
 #include "GameFramework/Character.h"
 #include "MyPlayerCharacter.generated.h"
 
+// 前方宣言
+class USpringArmComponent;
+class UCameraComponent;
+
 UCLASS()
 class SMITH_API AMyPlayerCharacter : public ACharacter
 {
@@ -26,10 +30,17 @@ public:
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
+	// スプリングアーム
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* m_pSpringArm;
+	// カメラ
+	UPROPERTY(VisibleAnywhere,BlueprintReadOnly,Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* m_pCamera;
 private:
 	void UpdatePawn();
 	void Pawn_MoveForward(float axisValue);
 	void Pawn_MoveRight(float axisValue);
 
 	FVector m_pos;
+	FRotator m_dir;
 };
