99ab1c5ab5bc570d49fdc9d1a2757ce1c2c8029f
ueno <email>
2025-01-23 00:51:26 +0900

    backup

--- Source/Smith/SmithActors/TurnMngObj/Private/SmithEnemy.cpp
+++ Source/Smith/SmithActors/TurnMngObj/Private/SmithEnemy.cpp
@@ -14,8 +14,8 @@ ASmithEnemy::ASmithEnemy()
   SetTurnPriority(ETurnPriority::Rival);
   PrimaryActorTick.bCanEverTick = false;
 
-  m_moveComp = CreateDefaultSubobject<USmithMoveComponent>(TEXT("konno Enemy Move Component"));
-  check((m_moveComp != nullptr));
+ m_moveComp = CreateDefaultSubobject<USmithMoveComponent>(TEXT("konno Enemy Move Component"));
+ check((m_moveComp != nullptr));
 
   m_moveComp->SetMoveSpeed(250.0f);
   MOVE_DISTANCE = 100.0f;
