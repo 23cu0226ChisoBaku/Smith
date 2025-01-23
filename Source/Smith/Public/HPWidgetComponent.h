746ea7971a09518b1ec461b461d1680d4e5f2c4f
麦 <>
2025-01-24 01:30:54 +0900

    back up

---
+++ Source/Smith/Public/HPWidgetComponent.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/WidgetComponent.h"
+#include "HPWidgetComponent.generated.h"
+
+class UHPWidget;
+/**
+ *
+ */
+UCLASS()
+class SMITH_API UHPWidgetComponent : public UWidgetComponent
+{
+	GENERATED_BODY()
+public:
+	UHPWidgetComponent();
+
+public:
+	void SetHP(float);
+
+private:
+	UPROPERTY()
+	TObjectPtr<UHPWidget> m_widget;
+};
