4b560b5f40070a1241e90a6f5cc0986b4cf0060d
23cu0139 <>
2025-02-12 17:03:31 +0900

    back up

--- Source/SmithGod/Private/SmithDungeonDamageCalculator.cpp
+++ Source/SmithGod/Private/SmithDungeonDamageCalculator.cpp
@@ -19,9 +19,18 @@ FBattleResult USmithDungeonDamageCalculator::CalculateDamage( const FBattleAttac
                                                               const FBattleDefenseParamHandle& defender) const
 {
   int32 levelDifference = attacker.Level - defender.Level;
-  // 攻撃力　＊　定数^(攻撃者レベル　ー　攻撃を喰らう者のレベル) 小数点切り捨て
-  int32 damage = FMath::FloorToInt32(StaticCast<double>(attacker.AttackPoint) * FMath::Pow(m_constant, StaticCast<double>(levelDifference))); 
+  int32 def = defender.DefensePoint;
+  if(def <= 0)
+  {
+    def = 1;
+  }
+  double defRatio = 1.0 - (StaticCast<double>(def) / StaticCast<double>(attacker.AttackPoint));
+  
+  // 攻撃力　＊　定数^(攻撃者レベル　ー　攻撃を喰らう者のレベル) *  小数点切り捨て
+  int32 damage = FMath::FloorToInt32(StaticCast<double>(attacker.AttackPoint) * FMath::Pow(m_constant, StaticCast<double>(levelDifference)) * defRatio); 
 
+  MDebug::LogError(TEXT("結果") + FString::SanitizeFloat(StaticCast<double>(attacker.AttackPoint) * FMath::Pow(m_constant, StaticCast<double>(levelDifference)) * defRatio));
+  MDebug::LogError(TEXT("結果") + FString::SanitizeFloat(defRatio));
   FBattleResult result;
   result.Damage = damage;
   return result;
